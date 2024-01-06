cb9f5f6158b8810da5220f7450b3e5002f9c6f19
umut can altıntaş <uavnot2001@gmail.com>
2024-01-07 00:11:17 +0300

    Token service private methodlar

---
+++ AuthServer.Service/Services/TokenService.cs
@@ -0,0 +1,81 @@
+﻿using AuthServer.Core.Configuration;
+using AuthServer.Core.Dtos;
+using AuthServer.Core.Models;
+using AuthServer.Core.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using SharedLibrary.Configurations;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Service.Services
+{
+    internal class TokenService : ITokenService
+    {
+
+        private readonly UserManager<UserApp> _userManager;
+
+        private readonly CustomTokenOption _customTokenOption;
+
+        public TokenService(UserManager<UserApp> userManager, IOptions<CustomTokenOption> customTokenOption)
+        {
+            _userManager = userManager;
+            _customTokenOption = customTokenOption.Value;
+        }
+
+        private string CreateRefreshToken()
+        {
+            var numberByte = new byte[32];
+
+            using var rnd = RandomNumberGenerator.Create();
+
+            rnd.GetBytes(numberByte);
+
+            return Convert.ToBase64String(numberByte);
+        }
+
+        private IEnumerable<Claim> GetClaims(UserApp userApp, List<String> audiences)
+        {
+            var userList = new List<Claim> { 
+                new Claim(ClaimTypes.NameIdentifier,userApp.Id),
+                new Claim(JwtRegisteredClaimNames.Email, userApp.Email),
+                new Claim(ClaimTypes.Name, userApp.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            userList.AddRange(audiences.Select(x => new Claim(JwtRegisteredClaimNames.Aud, x)));
+
+            return userList;
+        }
+
+        private IEnumerable<Claim> GetClaimsByClient(Client client) 
+        {
+            var claims = new List<Claim>();
+
+            claims.AddRange(client.Audiences.Select(x => new Claim(JwtRegisteredClaimNames.Aud, x)));
+
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString());
+            new Claim(JwtRegisteredClaimNames.Sub, client.Id.ToString());
+
+            return claims;
+
+        }
+
+        public TokenDto CreateToken(UserApp userApp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClientTokenDto CreateTokenByClient(Client client)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
