d69bdac788349567dee05be50c920948feabc139
umut can altıntaş <uavnot2001@gmail.com>
2025-05-29 21:59:29 +0300

    ProductController operations

---
+++ AuthServer.API/Controllers/ProductController.cs
@@ -0,0 +1,47 @@
+﻿using AuthServer.Core.Dtos;
+using AuthServer.Core.Models;
+using AuthServer.Core.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthServer.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : CustomBaseController
+    {
+
+        private readonly IServiceGeneric<Product, ProductDto> _serviceGeneric;
+
+        public ProductController(IServiceGeneric<Product, ProductDto> serviceGeneric)
+        {
+            _serviceGeneric = serviceGeneric;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            return CreateActionResult(await _serviceGeneric.GetAllAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveProduct(ProductDto productDto)
+        {
+            return CreateActionResult(await _serviceGeneric.AddAsync(productDto));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateProduct(ProductDto productDto)
+        {
+            return CreateActionResult(await _serviceGeneric.Update(productDto, productDto.Id));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            return CreateActionResult(await _serviceGeneric.Remove(id));
+        }
+    }
+}
