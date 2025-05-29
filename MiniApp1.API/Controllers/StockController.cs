b2c3fb4f4bfff73a3b1e59b724b9f1aac3104b82
umut can altıntaş <uavnot2001@gmail.com>
2025-05-29 23:23:47 +0300

    Mock Controllers authentication-authorization test modules

---
+++ MiniApp1.API/Controllers/StockController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace MiniApp1.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetStock()
+        {
+            var username = HttpContext.User.Identity.Name;
+
+            var userIdClaim = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+
+            return Ok("stock operations " + username + ", " + userIdClaim.Value);
+        }
+    }
+}
---
+++ MiniApp2.API/Controllers/FeeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace MiniApp2.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetFee()
+        {
+            var username = HttpContext.User.Identity.Name;
+
+            var userId = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+
+            return Ok("fee operations " + username + ", " + userId);
+        }
+    }
+}
