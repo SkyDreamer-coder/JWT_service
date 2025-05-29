48a590214324024e2e20a06396c09a17ef9e542b
umut can altıntaş <uavnot2001@gmail.com>
2025-05-29 15:38:44 +0300

    AuthController Endpoints implementations

---
+++ AuthServer.API/Controllers/AuthController.cs
@@ -0,0 +1,45 @@
+﻿using AuthServer.Core.Dtos;
+using AuthServer.Core.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SharedLibrary.Dtos;
+
+namespace AuthServer.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AuthController : CustomBaseController
+    {
+
+        private readonly IAuthenticationService _authenticationService;
+
+        public AuthController(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateToken(LoginDto loginDto)
+        {
+            return CreateActionResult(await _authenticationService.CreateTokenAsync(loginDto));
+        }
+
+        [HttpPost]
+        public IActionResult CreateTokenByClient(ClientLoginDto clientLoginDto)
+        {
+            return CreateActionResult(_authenticationService.CreateTokenByClient(clientLoginDto));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RevokeRefreshToken(RefreshTokenDto refreshTokenDto)
+        {
+            return CreateActionResult(await _authenticationService.RevokeRefreshToken(refreshTokenDto.RefreshToken));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTokenByRefreshToken(RefreshTokenDto refreshTokenDto)
+        {
+            return CreateActionResult(await _authenticationService.CreateTokenByRefreshToken(refreshTokenDto.RefreshToken));
+        }
+    }
+}
